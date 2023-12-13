d91f04cc5675f4224efdef42128ceaf97afe446f
Michele Scarlato <michele.scarlato@gmail.com>
2023-12-13 19:59:28 +0100

    Removed unused imports and commented text.

--- tests/students_tornado_tests_list_users.py
+++ tests/students_tornado_tests_list_users.py
@@ -1,13 +1,8 @@
-import asyncio
-import atexit
-import json
 import motor
-import requests
 import tornado.escape
 from tornado.web import Application
-from bson import ObjectId, json_util
 from students_tornado import MainHandler
-from tornado.testing import AsyncHTTPTestCase, AsyncTestCase, AsyncHTTPClient, gen_test
+from tornado.testing import AsyncHTTPTestCase
 import tornado.web
 
 uri = 'mongodb://root:example@172.20.0.2:27017/'
